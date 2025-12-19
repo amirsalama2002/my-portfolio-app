efa814c5cce06eaeeebc56dd28e85f678cb99c46
Amir Salama <hassanamirsalama.com>
2025-12-19 06:11:21 +0200

    gggggggggg

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
-import './index.css';
 import App from './App.jsx';
 import './i18n';
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
